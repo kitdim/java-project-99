c9c81c63cae8e2d83323fd7ef2d609c67b40216d
kitdim <kitaykin9808@gmail.com>
2024-04-08 12:31:03 +0300

    update

--- src/main/java/hexlet/code/controller/AuthenticationController.java
+++ src/main/java/hexlet/code/controller/AuthenticationController.java
@@ -2,22 +2,23 @@ package hexlet.code.controller;
 
 import hexlet.code.dto.AuthRequest;
 import hexlet.code.util.JWTUtils;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
-@RequestMapping(path = "/api/login")
-@AllArgsConstructor
+@RequestMapping("/api")
 public class AuthenticationController {
-    private JWTUtils jwtUtils;
-    private AuthenticationManager authenticationManager;
 
-    @PostMapping("")
-    @ResponseStatus(HttpStatus.OK)
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
     public String create(@RequestBody AuthRequest authRequest) {
         var authentication = new UsernamePasswordAuthenticationToken(
                 authRequest.getUsername(), authRequest.getPassword());
