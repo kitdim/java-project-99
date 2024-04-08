ec45a6636005559fa119318c722ea9fcffd1c48d
kitdim <kitaykin9808@gmail.com>
2024-04-08 12:38:53 +0300

    update

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -2,23 +2,27 @@ package hexlet.code.component;
 
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
-import hexlet.code.sevice.CustomUserDetailsService;
-import hexlet.code.util.UserUtils;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import hexlet.code.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
-public final class DataInitializer implements ApplicationRunner {
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+    private final UserRepository userRepository;
     private final CustomUserDetailsService userService;
-    private final UserUtils userUtils;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        User admin = userUtils.getAdmin();
-        userService.createUser(admin);
+        var email = "hexlet@example.com";
+        if (userRepository.findByEmail(email).isEmpty()) {
+            var userData = new User();
+            userData.setEmail(email);
+            userData.setPasswordDigest("qwerty");
+            userService.createUser(userData);
+        }
     }
 }
