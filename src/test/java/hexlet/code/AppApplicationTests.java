dfe2ac9898d274bb6202626b531da5e852ee3475
kitdim <kitaykin9808@gmail.com>
2024-04-14 13:42:53 +0300

    create init test

---
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppApplicationTests {
+    @Test
+    @DisplayName("Test init app")
+    void contextLoads() {
+
+    }
+}
