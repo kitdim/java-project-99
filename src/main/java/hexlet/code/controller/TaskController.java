f6e6093255c564f1391fdbe2e5178f2980af309b
kitdim <kitaykin9808@gmail.com>
2024-04-28 16:41:21 +0300

    add solution by route "GET"

--- src/main/java/hexlet/code/controller/TaskController.java
+++ src/main/java/hexlet/code/controller/TaskController.java
@@ -1,4 +1,31 @@
 package hexlet.code.controller;
 
+import hexlet.code.dto.task.TaskDTO;
+import hexlet.code.model.Task;
+import hexlet.code.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class TaskController {
+    private final TaskService taskService;
+
+    @GetMapping(value = "/tasks")
+    public ResponseEntity<List<TaskDTO>> index() {
+        List<TaskDTO> tasks = taskService.getAll();
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(tasks.size()))
+                .body(tasks);
+    }
+    @GetMapping(value = "/tasks/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public TaskDTO show(@PathVariable Long id) {
+        return taskService.findTask(id);
+    }
 }
--- src/main/java/hexlet/code/service/TaskService.java
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -1,4 +1,30 @@
 package hexlet.code.service;
 
+import hexlet.code.dto.task.TaskDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.TaskMapper;
+import hexlet.code.model.Task;
+import hexlet.code.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+    private final TaskMapper taskMapper;
+    private final TaskRepository taskRepository;
+
+    public List<TaskDTO> getAll() {
+        return taskRepository.findAll()
+                .stream()
+                .map(taskMapper::map)
+                .toList();
+    }
+    public TaskDTO findTask(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not Found: " + id));
+        return taskMapper.map(task);
+    }
 }
