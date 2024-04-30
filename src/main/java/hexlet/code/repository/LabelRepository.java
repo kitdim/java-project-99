b15eed96bbdb385f5e657d7199e6b5b2756bd861
kitdim <kitaykin9808@gmail.com>
2024-04-30 19:21:04 +0300

    add method find by name

--- src/main/java/hexlet/code/repository/LabelRepository.java
+++ src/main/java/hexlet/code/repository/LabelRepository.java
@@ -3,8 +3,10 @@ package hexlet.code.repository;
 import hexlet.code.model.Label;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface LabelRepository extends JpaRepository<Label, Long> {
     Set<Label> findByIdIn(Set<Long> labelIds);
+    Optional<Label> findByName(String name);
 }
