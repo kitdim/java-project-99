e440d92f6017c77ec53ab3984a4b35beadcbb165
kitdim <kitaykin9808@gmail.com>
2024-05-08 19:49:30 +0300

    update test

--- src/test/java/hexlet/code/api/UserControllerTest.java
+++ src/test/java/hexlet/code/api/UserControllerTest.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,7 +18,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-
 
 import java.util.HashMap;
 import java.util.List;
@@ -78,83 +75,91 @@ public class UserControllerTest {
         assertThat(users).containsAll(expected);
     }
 
-    @ParameterizedTest
-    @ValueSource(booleans = {true, false})
-    @DisplayName("Test find by id")
-    public void testShow(boolean isCorrectId) throws Exception {
-        Long id;
-        if (isCorrectId) {
-            id = testUser.getId();
-        } else {
-            id = 55L;
-        }
-
+    @Test
+    @DisplayName("Test find user by id")
+    public void testShow() throws Exception {
+        Long id = testUser.getId();
         MvcResult result = mockMvc.perform(get("/api/users/{id}", id).with(token))
                 .andDo(print())
                 .andReturn();
-
-        if (isCorrectId) {
-            UserDTO dto = objectMapper.readValue(result.getResponse().getContentAsString(), UserDTO.class);
-            assertEquals(testUser.getFirstName(), dto.getFirstName());
-            assertEquals(testUser.getLastName(), dto.getLastName());
-        } else {
-            assertEquals(404, result.getResponse().getStatus());
-        }
+        UserDTO dto = objectMapper.readValue(result.getResponse().getContentAsString(), UserDTO.class);
+        assertEquals(testUser.getFirstName(), dto.getFirstName());
+        assertEquals(testUser.getLastName(), dto.getLastName());
     }
 
-    @ParameterizedTest
-    @ValueSource(booleans = {true, false})
+    @Test
+    @DisplayName("Test find user with a bad id")
+    public void testShowWithBadId() throws Exception {
+        Long id = 55L;
+        MvcResult result = mockMvc.perform(get("/api/users/{id}", id).with(token))
+                .andDo(print())
+                .andReturn();
+        assertEquals(404, result.getResponse().getStatus());
+    }
+
+    @Test
     @DisplayName("Test create user")
-    public void testCreate(boolean isSuccess) throws Exception {
+    public void testCreate() throws Exception {
         User someUser = Instancio.of(testUtils.getUserModel()).create();
-        if (!isSuccess) {
-            someUser.setEmail("bad-email");
-            someUser.setPasswordDigest("21");
-        }
         MockHttpServletRequestBuilder request = post("/api/users")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(someUser)).with(token);
-        if (isSuccess) {
-            mockMvc.perform(request)
-                    .andExpect(status().isCreated());
-            User user = userRepository.findByEmail(someUser.getEmail()).get();
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
+        User user = userRepository.findByEmail(someUser.getEmail()).get();
 
-            assertNotNull(user);
-            assertThat(user.getFirstName()).isEqualTo(someUser.getFirstName());
-            assertThat(user.getLastName()).isEqualTo(someUser.getLastName());
-        } else {
-            mockMvc.perform(request)
-                    .andExpect(status().isInternalServerError());
-        }
+        assertNotNull(user);
+        assertThat(user.getFirstName()).isEqualTo(someUser.getFirstName());
+        assertThat(user.getLastName()).isEqualTo(someUser.getLastName());
     }
 
-    @ParameterizedTest
-    @ValueSource(booleans = {true, false})
+    @Test
+    @DisplayName("Test create with a bad value")
+    public void testCreateWithBadError() throws Exception {
+        User someUser = Instancio.of(testUtils.getUserModel()).create();
+        someUser.setEmail("bad-email");
+        someUser.setPasswordDigest("21");
+
+        MockHttpServletRequestBuilder request = post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(someUser)).with(token);
+        mockMvc.perform(request)
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
     @DisplayName("Test update user")
-    public void testUpdate(boolean isSuccess) throws Exception {
+    public void testUpdate() throws Exception {
         Long id = testUser.getId();
         HashMap<String, String> data = new HashMap<>();
         data.put("firstName", "Ivan");
-
-        if (!isSuccess) {
-            id = testUser.getId();
-            data.put("password", "11");
-            data.put("email", "bad-email");
-        }
 
         MockHttpServletRequestBuilder request = put("/api/users/{id}", id)
                 .with(token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(data));
 
-        if (isSuccess) {
-            mockMvc.perform(request).andExpect(status().isOk());
-            User someUser = userRepository.findById(testUser.getId()).get();
-            assertThat(someUser.getFirstName()).isEqualTo(("Ivan"));
-        } else {
-            mockMvc.perform(request)
-                    .andExpect(status().isInternalServerError());
-        }
+        mockMvc.perform(request).andExpect(status().isOk());
+        User someUser = userRepository.findById(testUser.getId()).get();
+        assertThat(someUser.getFirstName()).isEqualTo(("Ivan"));
+    }
+
+    @Test
+    @DisplayName("Test update user with a bad value")
+    public void testUpdateWithBadValue() throws Exception {
+        Long id = testUser.getId();
+        HashMap<String, String> data = new HashMap<>();
+        data.put("firstName", "Ivan");
+        data.put("password", "11");
+        data.put("email", "bad-email");
+
+        MockHttpServletRequestBuilder request = put("/api/users/{id}", id)
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isInternalServerError());
     }
 
     @Test
