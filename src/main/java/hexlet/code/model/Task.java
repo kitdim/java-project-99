c060d082f3a74a5da3c335c60bd4eeb542bc8937
kitdim <kitaykin9808@gmail.com>
2024-05-08 20:51:48 +0300

    synchronisation so that when a label is deleted/added to a task, the task in the label is also deleted

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -8,6 +8,8 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -61,6 +63,11 @@ public class Task implements BaseEntity {
     private User assignee;
 
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JoinTable(
+            name = "tasklabel",
+            joinColumns = @JoinColumn(name = "taskid"),
+            inverseJoinColumns = @JoinColumn(name = "labelid")
+    )
     private Set<Label> labels = new HashSet<>();
 
     @Column(name = "created_at")
