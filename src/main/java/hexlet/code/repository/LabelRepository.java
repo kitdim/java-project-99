b446c84a66ce4f45b5d895d9e1088b33c198e3d7
kitdim <kitaykin9808@gmail.com>
2024-05-09 18:49:22 +0300

    add annotation @Repository

--- src/main/java/hexlet/code/repository/LabelRepository.java
+++ src/main/java/hexlet/code/repository/LabelRepository.java
@@ -2,10 +2,12 @@ package hexlet.code.repository;
 
 import hexlet.code.model.Label;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.Set;
 
+@Repository
 public interface LabelRepository extends JpaRepository<Label, Long> {
     Set<Label> findByIdIn(Set<Long> labelIds);
     Optional<Label> findByName(String name);
