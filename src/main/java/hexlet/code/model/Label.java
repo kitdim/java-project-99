aeb0316bd1e841bc741077ec3efee519503bddb9
kitdim <kitaykin9808@gmail.com>
2024-05-09 18:50:08 +0300

    add annotation @Table(name = "labels")

--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -1,6 +1,15 @@
 package hexlet.code.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
